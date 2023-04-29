749ad03aca2ae275e6597fd793e5221d9f182f72
dlalsdn6166 <dlalsdn6166@naver.com>
2023-04-29 15:41:19 +0900

    Music stream
    서버에서 음악 스트리밍
    
    음악 에셋 제거
    음악 패키지 변경
    도서 정보 검색 서버로 이전

---
+++ lib/NLP/env/env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const apiKey = '"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”';
+}
